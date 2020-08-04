050088346f6158f5a447fe2320dca1035d9aba73
Rafael <55772206+Rebel-Jedi@users.noreply.github.com>
2020-08-04 12:07:02 -0500

    test file

---
+++ Covid-19 Helper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-19 Helper
+//
+//  Created by Rafael Avila on 8/3/20.
+//  Copyright © 2020 Rafael Avila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
